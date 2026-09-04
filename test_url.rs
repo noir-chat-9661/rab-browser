2d341019de1b4b395963a6d3d436a094ea7229b6
凶兆の黒猫 <102592819+noir-chat-9661@users.noreply.github.com>
2026-09-04 18:26:23 +0900

    chore: レビュー検証用のスクラッチファイルを削除(誤コミット)

--- test_url.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let url = url::Url::parse("https://[::1]:8080").unwrap();
-    println!("host_str: {:?}", url.host_str());
-    println!("ip parse: {:?}", url.host_str().unwrap().parse::<std::net::IpAddr>());
-}
--- test_url2.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let url = url::Url::parse("https://[::1]:8080").unwrap();
-    println!("host: {:?}", url.host());
-}
